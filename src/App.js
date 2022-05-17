b71709841233be2fa328a64c921369ac8874cc30
RCAbney <ryan@rcabney.com>
2022-05-16 22:06:59 -0400

    Add redux-persist and configure store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Papa from "papaparse";
 import { setAllBooths } from "./app/reducers/allBooths";
@@ -7,36 +7,39 @@ import Header from "./components/Header";
 
 function App() {
   const dispatch = useDispatch();
+  const allBooths = useSelector((state) => state.allBooths.booths);
 
   useEffect(() => {
-    async function getData() {
-      const response = await fetch("/data/gencon21-test.csv", {
-        method: "get",
-        headers: {
-          "content-type": "text/csv;charset=UTF-8",
-        },
-      });
-      const reader = response.body.getReader();
-      const result = await reader.read();
-      const decoder = new TextDecoder("utf-8");
-      const csv = decoder.decode(result.value);
-      const results = Papa.parse(csv, { header: true });
-      const rows = results.data;
-      const sorted = rows.sort((a, b) => {
-        const pubA = a.Publisher?.toUpperCase() || "";
-        const pubB = b.Publisher?.toUpperCase() || "";
-        if (pubA < pubB) {
-          return -1;
-        }
-        if (pubA > pubB) {
-          return 1;
-        }
-        return 0;
-      });
-      dispatch(setAllBooths(sorted));
+    if (allBooths.length === 0) {
+      async function getData() {
+        const response = await fetch("/data/gencon21-test.csv", {
+          method: "get",
+          headers: {
+            "content-type": "text/csv;charset=UTF-8",
+          },
+        });
+        const reader = response.body.getReader();
+        const result = await reader.read();
+        const decoder = new TextDecoder("utf-8");
+        const csv = decoder.decode(result.value);
+        const results = Papa.parse(csv, { header: true });
+        const rows = results.data;
+        const sorted = rows.sort((a, b) => {
+          const pubA = a.Publisher?.toUpperCase() || "";
+          const pubB = b.Publisher?.toUpperCase() || "";
+          if (pubA < pubB) {
+            return -1;
+          }
+          if (pubA > pubB) {
+            return 1;
+          }
+          return 0;
+        });
+        dispatch(setAllBooths(sorted));
+      }
+      getData();
     }
-    getData();
-  }, [dispatch]);
+  }, [dispatch, allBooths.length]);
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import store from "./app/store";
 import App from "./App";
 import AllBooths from "./routes/AllBooths";
 import MyBooths from "./routes/MyBooths";
@@ -11,18 +13,21 @@ import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="all-booths" element={<AllBooths />} />
-            <Route path="my-booths" element={<MyBooths />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="all-booths" element={<AllBooths />} />
+              <Route path="my-booths" element={<MyBooths />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
